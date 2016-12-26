02b82e6e34f56dfdd60446a0cdc190598342422e
sunlandong <271839665@qq.com>
2016-12-26 17:41:16 +0800

    系统管理

---
+++ client/store/modules/menu/index.js
@@ -0,0 +1,46 @@
+import * as types from '../../mutation-types'
+// import lazyLoading from './lazyLoading'
+// import charts from './charts'
+// import uifeatures from './uifeatures'
+// import components from './components'
+// import tables from './tables'
+import system from './system'
+
+// show: meta.label -> name
+// name: component name
+// meta.label: display label
+// 这个对象需要从后台获取
+const state = {
+  items: [
+    // {
+    //   name: 'Dashboard',
+    //   path: '/dashboard',
+    //   meta: {
+    //     icon: 'fa-tachometer'
+    //   },
+    //   component: lazyLoading('dashboard', true)
+    // },
+    // charts,
+    // uifeatures,
+    // components,
+    // tables
+    system
+  ]
+}
+
+const mutations = {
+  [types.EXPAND_MENU] (state, menuItem) {
+    if (menuItem.index > -1) {
+      if (state.items[menuItem.index] && state.items[menuItem.index].meta) {
+        state.items[menuItem.index].meta.expanded = menuItem.expanded
+      }
+    } else if (menuItem.item && 'expanded' in menuItem.item.meta) {
+      menuItem.item.meta.expanded = menuItem.expanded
+    }
+  }
+}
+
+export default {
+  state,
+  mutations
+}
---
+++ client/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="content has-text-centered">
+    <p>
+      <img width="200" src="~assets/logo.svg" :alt="description">
+    </p>
+
+    <h1 class="is-title is-bold">{{ name.replace('-', ' ') }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return this.$store.state.pkg
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.is-title {
+  text-transform: capitalize;
+}
+</style>
